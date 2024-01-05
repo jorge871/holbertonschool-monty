a1768b74515f3f9e757d763b7efa081b04f78cbe
jorge871 <jg303114@gmail.com>
2024-01-05 19:51:01 -0300

    changed because it had syntax errors

--- principal_functions.c
+++ principal_functions.c
@@ -8,18 +8,40 @@
 */
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *push_stack;
-    char *arg = strtok(NULL, " \n");
+	stack_t *new_node = NULL, *temporal = *stack;
 
-    if (!arg || !is_integer(arg))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (cb->mode)
+	{
+		my_push(stack, line_number);
+		return;
+	}
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
 
-    int value = atoi(arg);
-
-    push_stack(*stack, value);
+	if (is_integer(cb->passed_arguments[1]))
+	{
+		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
+		free_all(stack);
+		free(new_node);
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = atoi(cb->passed_arguments[1]);
+	new_node->next = NULL;
+	if (!(*stack))
+	{
+		new_node->prev = *stack;
+		*stack = new_node;
+		return;
+	}
+	while (temporal->next)
+		temporal = temporal->next;
+	temporal->next = new_node;
+	new_node->prev = temporal;
 }
 /**
  * pall - This function prints all values in ascending order.
