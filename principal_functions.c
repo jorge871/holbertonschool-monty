08fed41d84c586eea3cdb0cff69ba9c930f5c3d9
jorge871 <jg303114@gmail.com>
2024-01-04 23:44:36 -0300

    Create file of the main functions

---
+++ principal_functions.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * push - This function is used to push a value.
+ * @stack: Stack is address of the linked lists.
+ * @line_number: This is the number of line.
+*/
+void push(stack_t **stack, unsigned int line_number)
+{
+
+};
+
+/**
+ * pall - This function prints all values in ascending order.
+ * @stack: Stack is address of the linked lists.
+ * @line_number: This is the number of line.
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+
+};
+
+/**
+ * pop - Function that removes the top element from the stack.
+ * @stack: Stack is address of the linked lists.
+ * @line_number: This is the number of line.
+*/
+void pop(stack_t **stack, unsigned int line_number)
+{
+
+};
